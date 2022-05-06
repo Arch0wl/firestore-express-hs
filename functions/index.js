e18f53fa5dfffbfe55abf06305c31842c06cc84f
Arch0wl <annstaroviotov@gmail.com>
2022-05-06 11:46:55 -0400

    changes

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions");
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.send('This is working');
+});
+
+app.get('/', (req, res) => {
+    res.send('This is Home');
+});
+
+exports.app = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
